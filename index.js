a68dc3ed674789837dc4768511c9860249594821
Josh Birdwell <jbirdwell34@gmail.com>
2017-08-14 21:29:24 -0500

    Changes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import router from './router';
 
 mongoose.Promise = global.Promise;
 
-mongoose.createConnection('mongodb://admin:CetrK5t5J5ATVS8t@cluster0-shard-00-00-3utv2.mongodb.net:27017,cluster0-shard-00-01-3utv2.mongodb.net:27017,cluster0-shard-00-02-3utv2.mongodb.net:27017/main?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin', {
+mongoose.connect('mongodb://admin:CetrK5t5J5ATVS8t@cluster0-shard-00-00-3utv2.mongodb.net:27017,cluster0-shard-00-01-3utv2.mongodb.net:27017,cluster0-shard-00-02-3utv2.mongodb.net:27017/main?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin', {
   useMongoClient: true,
 });
 
